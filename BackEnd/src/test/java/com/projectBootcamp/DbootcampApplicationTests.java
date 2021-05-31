919d37d182220e7600b15fe5892b195230b09ae6
Brutos212 <brutos212@gmail.com>
2021-05-30 23:59:39 -0300

    Integrando a API

---
+++ BackEnd/src/test/java/com/projectBootcamp/DbootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectBootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
